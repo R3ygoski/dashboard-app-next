9257295d3ee6cb88796200aa792a477cce87694a
R3ygoski <hckranonymous2@hotmail.com>
2024-12-05 22:46:16 -0300

    Setup: Github Pages for Next

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/dashboard-app-next",
+  assetPrefix: "/dashboard-app-next/",
 };
 
 export default nextConfig;
